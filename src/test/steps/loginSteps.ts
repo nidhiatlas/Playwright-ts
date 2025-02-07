1f6409137d829262aa106807bfdeeb2dac93a212
nidhi <nidhi.jaiswal@external.atlascopco.com>
2025-02-07 15:20:03 +0530

    First

--- src/test/steps/loginSteps.ts
+++ src/test/steps/loginSteps.ts
@@ -4,7 +4,7 @@ Given('User navigates on the application', async function () {
     
     
   });
-Given('I am on the OrangeHRM login page', async function () {
+Given('User is on the OrangeHRM login page', async function () {
     // Write code here that turns the phrase above into concrete actions
   
   });
