c7e4eb86044561c8fedcbbbdad1ba61460d02bcb
nidhi <nidhi.jaiswal@external.atlascopco.com>
2025-02-11 09:14:38 +0530

    First

---
+++ src/test/tests/allure1.spec.ts
@@ -0,0 +1,5 @@
+import {test,expect} from "@playwright/test";
+test("practice 1",async({page})=>{
+    await page.goto("https://opensource-demo.orangehrmlive.com/");
+    await expect(page).toHaveTitle("OrangeHRM");
+})
