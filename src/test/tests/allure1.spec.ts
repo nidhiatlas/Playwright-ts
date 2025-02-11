3c601fc1ce342d00dabbbf10eb2cda3a39f64954
nidhi <nidhi.jaiswal@external.atlascopco.com>
2025-02-11 14:00:45 +0530

    First

--- src/test/tests/allure1.spec.ts
+++ src/test/tests/allure1.spec.ts
@@ -3,3 +3,7 @@ test("practice 1",async({page})=>{
     await page.goto("https://opensource-demo.orangehrmlive.com/");
     await expect(page).toHaveTitle("OrangeHRM");
 })
+test("practice 2",async({page})=>{
+    await page.goto("https://opensource-demo.orangehrmlive.com/");
+    await expect(page).toHaveTitle("OrangeHRM12");
+})
